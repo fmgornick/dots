f37029a396f991fa6509388276e4ae72b23831d2
Fletcher Gornick <fletchermgornick@gmail.com>
2023-07-07 00:45:47 -0500

    add emacs config

---
+++ .config/emacs/init.el
@@ -0,0 +1,15 @@
+(require 'org)
+(org-babel-load-file "~/.config/emacs/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elfeed-feeds
+   '("http://www.reddit.com/r/emacs/.rss" "http://rss.art19.com/the-daily" "https://www2.lib.uchicago.edu/keith/emacs/feed.xml")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
