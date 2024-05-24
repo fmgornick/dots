a0e686e78dfecf90a310dbcb17d595b4398d4d23
Fletcher Gornick <fletchermgornick@gmail.com>
2024-05-23 22:02:58 -0500

    blah

--- .config/nvim/lua/fmgornick/plugins/oil.lua
+++ .config/nvim/lua/fmgornick/plugins/oil.lua
@@ -5,16 +5,20 @@ return {
   keys = { { "<leader>e", "<cmd>Oil<cr>", desc = "parent dir", mode = { "n", "v" } } },
   opts = {
     default_file_explorer = true,
+    skip_confirm_for_simple_edits = true,
+    prompt_save_on_select_new_entry = true,
+    cleanup_delay_ms = false,
     view_options = {
       show_hidden = true,
     },
     keymaps = {
       ["g?"] = "actions.show_help",
       ["<cr>"] = "actions.select",
-      ["<c-s>"] = "actions.select_split",
-      ["<c-v>"] = "actions.select_vsplit",
-      ["<c-p>"] = "actions.preview",
+      ["<c-w>s"] = "actions.select_split",
+      ["<c-w>v"] = "actions.select_vsplit",
       ["-"] = "actions.parent",
+      ["<leader>p"] = "actions.preview",
+      ["H"] = "actions.toggle_hidden",
       q = "actions.close",
     },
     use_default_keymaps = false,
