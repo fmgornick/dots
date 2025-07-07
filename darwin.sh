d1d38bfc61280245df974b45c58eae0044a33e7e
Fletcher Gornick <fletcher@gornick.dev>
2025-07-06 20:24:15 -0500

    add symlink scripts

---
+++ darwin.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ln  -sf  ~/.config/zsh/config.zsh              ~/.zshrc
+ln  -sf  ~/repos/dots/clang-format/config.yml  ~/.clang-format
+ln  -s   ~/repos/dots/alacritty-darwin         ~/.config/alacritty
+ln  -s   ~/repos/dots/emacs                    ~/.config/emacs
+ln  -s   ~/repos/dots/git                      ~/.config/git
+ln  -s   ~/repos/dots/nvim                     ~/.config/nvim
+ln  -s   ~/repos/dots/tmux                     ~/.config/tmux
+ln  -s   ~/repos/dots/zsh                      ~/.config/zsh
---
+++ linux.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ln  -sf  ~/.config/bash/config.bash            ~/.bashrc
+ln  -sf  ~/.config/bash/profile.bash           ~/.bash_profile
+ln  -sf  ~/repos/dots/clang-format/config.yml  ~/.clang-format
+ln  -s   ~/repos/dots/alacritty-linux          ~/.config/alacritty
+ln  -s   ~/repos/dots/bash                     ~/.config/bash
+ln  -s   ~/repos/dots/git                      ~/.config/git
+ln  -s   ~/repos/dots/nvim                     ~/.config/nvim
