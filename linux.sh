fff537b8d48602f54477ef9c4a2cddb842c5bcdb
Fletcher Gornick <fletcher@gornick.dev>
2025-07-07 12:04:32 -0500

    add fuzzel symlink

--- linux.sh
+++ linux.sh
@@ -5,6 +5,7 @@ ln  -sf  ~/.config/bash/profile.bash           ~/.bash_profile
 ln  -sf  ~/repos/dots/clang-format/config.yml  ~/.clang-format
 ln  -s   ~/repos/dots/alacritty-linux          ~/.config/alacritty
 ln  -s   ~/repos/dots/bash                     ~/.config/bash
+ln  -s   ~/repos/dots/fuzzel                   ~/.config/fuzzel
 ln  -s   ~/repos/dots/git                      ~/.config/git
 ln  -s   ~/repos/dots/nvim                     ~/.config/nvim
 ln  -s   ~/repos/dots/sway                     ~/.config/sway
