7eeeac95b0b289ca6df1871e34f5615a79c290c5
Fletcher Gornick <fletcher@gornick.dev>
2025-08-08 23:17:45 -0500

    scripts: remove emacs symlink

--- darwin.sh
+++ darwin.sh
@@ -3,7 +3,6 @@
 rm  -rf  ~/.zshrc
 rm  -rf  ~/.clang-format
 rm  -rf  ~/.config/alacritty
-rm  -rf  ~/.config/emacs
 rm  -rf  ~/.config/git
 rm  -rf  ~/.config/nvim
 rm  -rf  ~/.config/tmux
@@ -12,7 +11,6 @@ rm  -rf  ~/.config/zsh
 ln  -sf  ~/.config/zsh/config.zsh        ~/.zshrc
 ln  -sf  $(pwd)/clang-format/config.yml  ~/.clang-format
 ln  -s   $(pwd)/alacritty-darwin         ~/.config/alacritty
-ln  -s   $(pwd)/emacs                    ~/.config/emacs
 ln  -s   $(pwd)/git                      ~/.config/git
 ln  -s   $(pwd)/nvim                     ~/.config/nvim
 ln  -s   $(pwd)/tmux                     ~/.config/tmux
