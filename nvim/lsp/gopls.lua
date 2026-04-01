1b479496d528dd54af4f01dd57e3324fae628df1
Fletcher Gornick <fletcher.gornick@target.com>
2026-03-31 22:56:07 -0500

    update gopls to add syntax highlighting

--- nvim/lsp/gopls.lua
+++ nvim/lsp/gopls.lua
@@ -1,11 +1,11 @@
 return {
     cmd = { "gopls" },
-    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+    filetypes = { "go", "gomod" },
     root_markers = { "go.mod", "go.sum", ".git" },
     settings = {
         gopls = {
-            codelenses = { test = true, tidy = true, vendor = true },
-            usePlaceholders = true,
+            codelenses = { test = true },
+            semanticTokens = true,
         },
     },
 }
